423137bf3df7cc5fd87c25e56892aeeaddffcdd2
Sandy Eka <sandy.eka.nuggroho@gmail.com>
2020-08-23 17:02:22 +0700

    Initial Commit. Added index.php with Content HTML 5 and Text

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning GIT</title>
+</head>
+<body>
+    Hy There, I am Learning GIT from Dicoding.com
+</body>
+</html>
